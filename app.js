37eee0ff385da517d878b6f8b430956c9a87e176
mayrasillas <84360847+mayrasillas@users.noreply.github.com>
2021-05-17 21:27:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,15 @@
            * pendiente
          )
 */
+const fs = require('fs')
 
 exports.leerTareas = function () {
-    /*
-        En esta función debes leer y retornar las tareas registradas.
-    */
+    const lea = fs.readFileSync('tareas.json', 'utf-8')
+    return JSON.parse(lea)
 }
 
 exports.agregarTarea = function (tarea) {
-    /*
-        Registra y guarda una nueva tarea.
-    */
+    fs.writeFileSync('../prueba-js-dh/tareas.json', JSON.stringify(tarea));
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -24,4 +22,8 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+        const filtro = fs.readFileSync('tareas.json', 'utf-8')
+        if(filtro.status == estado){
+            return JSON.parse(filtro)
+        }
 }
